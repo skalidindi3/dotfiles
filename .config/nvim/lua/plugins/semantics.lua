81ebf42123f1a26e6a89fb319071ec5eee210bc6
Sai Kalidindi <skalidindi3@gmail.com>
2025-08-28 13:04:35 -0400

    nvim/tmux revamp p38: add flash.nvim

--- .config/nvim/lua/plugins/semantics.lua
+++ .config/nvim/lua/plugins/semantics.lua
@@ -7,4 +7,22 @@ return {
         version = "*",
         opts = {},
     },
+
+    -- enable jumping & treesitter visual selection
+    {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        opts = {
+            modes = {
+                char = {
+                    enabled = false,
+                },
+            },
+        },
+        -- stylua: ignore
+        keys = {
+            { "<leader>j", mode = { "n", "o" }, function() require("flash").jump() end, desc = "quick jump" },
+            { "<leader>v", mode = { "n", "o" }, function() require("flash").treesitter() end, desc = "treesitter visual selection" },
+        },
+    },
 }
