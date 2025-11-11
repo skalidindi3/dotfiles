f47f10905076038d4b3a065516ee8bef68e30891
Sai Kalidindi <skalidindi3@gmail.com>
2025-11-10 19:16:05 -0500

    nvim/tmux revamp p73: replace mpd with remote conn stats iff needed

---
+++ .config/tmux/scripts/ssh-conn-info.sh
@@ -0,0 +1,42 @@
+#!/bin/zsh
+
+# NOTE: keeping the below for bookkeeping purposes
+# client_pid=$(tmux display-message -p '#{client_pid}')
+# ssh_check=$(pstree -s $client_pid | grep sshd &> /dev/null)
+# ssh_connected=$?
+
+client_pty=$(tmux display-message -p '#{client_tty}')
+remote_controls_tmux_pty_check=$(w | grep ${client_pty:5})  # NOTE: strips "/dev/" prefix
+remote_controls_tmux_pty=$?
+
+if (( remote_controls_tmux_pty == 0 )); then
+    remote_ip=$(echo $remote_controls_tmux_pty_check | awk '{print $3}')
+    remote_name_check=$(nslookup $remote_ip | grep --color=never name)
+    remote_name_exists=$?
+
+    echo -n "#[fg=#{@nordic_gray4},bg=#{@nordic_black0},nobold]$client_pty"
+    if (( remote_name_exists == 0 )); then
+        remote_name_full=$(echo $remote_name_check | sed 's/.*name = //')
+        remote_name_base=$(echo $remote_name_full | sed 's/\..*//')
+
+        if echo $remote_name_full | grep -E '\.lan|\.local' &>/dev/null; then
+            # connected from LAN
+            echo -n "#[fg=#{@host_color},bg=#{@nordic_black0},nobold] [lan] "
+            echo "#[fg=#{@nordic_black0},bg=#{@host_color},bold] $remote_name_base "
+        elif echo $remote_name_full | grep -E '\.ts\.' &>/dev/null; then
+            # connected over tailscale
+            echo -n "#[fg=#{@host_color},bg=#{@nordic_black0},nobold] [tailnet] "
+            echo "#[fg=#{@nordic_black0},bg=#{@host_color},bold] $remote_name_base "
+        else
+            # WARN: unknown named connection
+            echo -n "#[fg=#{@nordic_red_bright},bg=#{@nordic_black0},nobold] [???] "
+            echo "#[fg=#{@nordic_black0},bg=#{@nordic_red_bright},bold] $remote_name_full "
+        fi
+    else
+        # WARN: unknown IP connected
+        echo -n "#[fg=#{@nordic_red_bright},bg=#{@nordic_black0},nobold] [nodns] "
+        echo "#[fg=#{@nordic_black0},bg=#{@nordic_red_bright},bold] $remote_ip "
+    fi
+else
+    (exit 1)
+fi
