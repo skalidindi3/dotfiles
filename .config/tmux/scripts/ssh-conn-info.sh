fdcfed3eab55b3d5d502f6813fe0b9a895e2647f
Sai Kalidindi <skalidindi3@gmail.com>
2025-12-10 16:44:25 -0500

    nvim/tmux revamp p79: fix ssh-conn-info.sh for tailnet

--- .config/tmux/scripts/ssh-conn-info.sh
+++ .config/tmux/scripts/ssh-conn-info.sh
@@ -19,26 +19,25 @@ if (( remote_controls_tmux_pty == 0 )); then
     remote_name_check=$(nslookup $remote_ip | grep --color=never name)
     remote_name_exists=$?
 
-    echo -n "#[fg=#{@nordic_gray4},bg=#{@nordic_black0},nobold]$client_pty"
     if (( remote_name_exists == 0 )); then
         remote_name_full=$(echo $remote_name_check | sed 's/.*name = //')
-        remote_name_base=$(echo $remote_name_full | sed 's/\..*//')
-
-        if echo $remote_name_full | grep -E '\.lan|\.local' &>/dev/null; then
-            # connected from LAN
-            echo -n "#[fg=#{@host_color},bg=#{@nordic_black0},nobold] [lan] "
-            echo "#[fg=#{@nordic_black0},bg=#{@host_color},bold] $remote_name_base "
-        elif echo $remote_name_full | grep -E '\.ts\.' &>/dev/null; then
-            # connected over tailscale
-            echo -n "#[fg=#{@host_color},bg=#{@nordic_black0},nobold] [tailnet] "
-            echo "#[fg=#{@nordic_black0},bg=#{@host_color},bold] $remote_name_base "
-        else
-            # WARN: unknown named connection
-            echo -n "#[fg=#{@nordic_red_bright},bg=#{@nordic_black0},nobold] [???] "
-            echo "#[fg=#{@nordic_black0},bg=#{@nordic_red_bright},bold] $remote_name_full "
-        fi
     else
-        # WARN: unknown IP connected
+        remote_name_full=$remote_ip
+    fi
+    remote_name_base=$(echo $remote_name_full | sed 's/\..*//')
+
+    echo -n "#[fg=#{@nordic_gray4},bg=#{@nordic_black0},nobold]$client_pty"
+
+    if echo $remote_name_full | grep -E '\.lan|\.local' &>/dev/null; then
+        # connected from LAN
+        echo -n "#[fg=#{@host_color},bg=#{@nordic_black0},nobold] [lan] "
+        echo "#[fg=#{@nordic_black0},bg=#{@host_color},bold] $remote_name_base "
+    elif echo $remote_name_full | grep -E '\.ts\.' &>/dev/null; then
+        # connected over tailscale
+        echo -n "#[fg=#{@host_color},bg=#{@nordic_black0},nobold] [tailnet] "
+        echo "#[fg=#{@nordic_black0},bg=#{@host_color},bold] $remote_name_base "
+    else
+        # WARN: unknown connection
         echo -n "#[fg=#{@nordic_red_bright},bg=#{@nordic_black0},nobold] [nodns] "
         echo "#[fg=#{@nordic_black0},bg=#{@nordic_red_bright},bold] $remote_ip "
     fi
