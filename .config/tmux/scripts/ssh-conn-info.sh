a3efc8896a47b1d0b23e459a448e3080a216f61e
Sai Kalidindi <skalidindi3@gmail.com>
2026-02-01 12:40:07 -0500

    nvim/tmux revamp p80: fix ssh-conn-info.sh for edge cases / ipv6

--- .config/tmux/scripts/ssh-conn-info.sh
+++ .config/tmux/scripts/ssh-conn-info.sh
@@ -5,42 +5,44 @@
 # ssh_check=$(pstree -s $client_pid | grep sshd &> /dev/null)
 # ssh_connected=$?
 
-if [[ $(uname) == Linux ]]; then
-    pty_strip=5  # NOTE: to remove "/dev/" prefix from linux
-else
-    pty_strip=8  # NOTE: to remove "/dev/tty" prefix from macOS
-fi
+# get tty of the current tmux client (not pane)
 client_pty=$(tmux display-message -p '#{client_tty}')
-remote_controls_tmux_pty_check=$(w | grep ${client_pty:$pty_strip})
-remote_controls_tmux_pty=$?
+# strip "/dev/" prefix
+stripped_pty=${client_pty:5}
+# check who is controlling the tty
+who_pty=$(who | grep $stripped_pty)
+# grab remote ip in brackets
+remote_ip=$(echo $who_pty| awk -F'[()]' '{print $2}')
 
-if (( remote_controls_tmux_pty == 0 )); then
-    remote_ip=$(echo $remote_controls_tmux_pty_check | awk '{print $3}')
-    remote_name_check=$(nslookup $remote_ip | grep --color=never name)
-    remote_name_exists=$?
-
-    if (( remote_name_exists == 0 )); then
-        remote_name_full=$(echo $remote_name_check | sed 's/.*name = //')
-    else
-        remote_name_full=$remote_ip
-    fi
-    remote_name_base=$(echo $remote_name_full | sed 's/\..*//')
-
+if [[ -n $remote_ip ]]; then
     echo -n "#[fg=#{@nordic_gray4},bg=#{@nordic_black0},nobold]$client_pty"
-
-    if echo $remote_name_full | grep -E '\.lan|\.local' &>/dev/null; then
-        # connected from LAN
-        echo -n "#[fg=#{@host_color},bg=#{@nordic_black0},nobold] [lan] "
-        echo "#[fg=#{@nordic_black0},bg=#{@host_color},bold] $remote_name_base "
-    elif echo $remote_name_full | grep -E '\.ts\.' &>/dev/null; then
+    case $remote_ip in
+        192.168.86.1)
+            # WARN: edge case for connecting via router forwarded port while using local exit node
+            echo -n "#[fg=#{@nordic_yellow},bg=#{@nordic_black0},nobold] [tailnet] "
+            echo "#[fg=#{@nordic_black0},bg=#{@nordic_yellow},bold] Google WiFi "
+            ;;
+        # connected over LAN
+        192.168.86.*)
+            # query mDNS for IP
+            remote_name=$(dig +short -x $remote_ip @224.0.0.251 -p 5353)
+            remote_name_base=${remote_name%%.*}
+            echo -n "#[fg=#{@host_color},bg=#{@nordic_black0},nobold] [lan] "
+            echo "#[fg=#{@nordic_black0},bg=#{@host_color},bold] $remote_name_base "
+            ;;
         # connected over tailscale
-        echo -n "#[fg=#{@host_color},bg=#{@nordic_black0},nobold] [tailnet] "
-        echo "#[fg=#{@nordic_black0},bg=#{@host_color},bold] $remote_name_base "
-    else
-        # WARN: unknown connection
-        echo -n "#[fg=#{@nordic_red_bright},bg=#{@nordic_black0},nobold] [nodns] "
-        echo "#[fg=#{@nordic_black0},bg=#{@nordic_red_bright},bold] $remote_ip "
-    fi
+        100.64.0.*|fd7a:115c:a1e0::*)
+            remote_name=$(tailscale whois --json $remote_ip | jq -r .Node.Name)
+            remote_name_base=${remote_name%%.*}
+            echo -n "#[fg=#{@host_color},bg=#{@nordic_black0},nobold] [tailnet] "
+            echo "#[fg=#{@nordic_black0},bg=#{@host_color},bold] $remote_name_base "
+            ;;
+        *)
+            # WARN: unknown connection
+            echo -n "#[fg=#{@nordic_red_bright},bg=#{@nordic_black0},nobold] [nodns] "
+            echo "#[fg=#{@nordic_black0},bg=#{@nordic_red_bright},bold] $remote_ip "
+            ;;
+    esac
 else
     (exit 1)
 fi
